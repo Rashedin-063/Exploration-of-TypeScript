1af3fb821965fd3160423d322e0696f9363f8938
RU063 <rashedinislam.06@gmail.com>
2024-11-02 14:43:58 +0600

    variable in typescript practiced

---
+++ src/variables.ts
@@ -0,0 +1,37 @@
+let awesomeName: string = 'shakeAndBake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase();
+
+// console.log(awesomeName)
+
+// awesomeName = 20
+
+let amount: number = 20;
+amount = 12 - 1;
+
+let isAwesome: boolean = true;
+isAwesome = false;
+
+// isAwesome = 'shakeAndBake'
+
+// console.log(amount, isAwesome)
+
+// challenge
+const stringVar: string = 'string variable'
+
+// console.log(stringVar.toLocaleUpperCase())
+
+const numVar: number = 12.3454343434
+
+// console.log(numVar.toFixed(2))
+
+let boolVar: boolean = false;
+
+console.log(boolVar.valueOf() === true ? 'Truthy' : 'Falsy');
+console.log(!boolVar)
+
+
+
+
+
+
