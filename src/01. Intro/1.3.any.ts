2cd7d8e35429f6821d1be97f16ffa2565c95b247
RU063 <rashedinislam.06@gmail.com>
2024-11-02 14:58:32 +0600

    any type practiced

---
+++ src/01. Intro/1.3.any.ts
@@ -0,0 +1,14 @@
+let notSure: any = 4;
+
+notSure = 'maybe a string instead';
+
+notSure = false;
+
+console.log(notSure);
+
+let random;
+
+random = 'boolean';
+random = true;
+
+console.log(random);
---
+++ src/tutorial.ts
@@ -0,0 +1,3 @@
+const books = ['1984', 'Brave New World', 'Fahrenheit 451']
+
+let foundBook
