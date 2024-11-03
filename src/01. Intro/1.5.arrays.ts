22482703dfebc56d75f534e74fe1de53f8ce5bb2
RU063 <rashedinislam.06@gmail.com>
2024-11-03 10:27:08 +0600

    array type practiced

---
+++ src/01. Intro/1.5.arrays.ts
@@ -0,0 +1,29 @@
+let prices: number[] = [1, 2, 3, 4, 5, 6];
+
+prices.push(15);
+// prices.push('15')
+
+console.log(prices);
+
+let fruits: string[] = ['apple', 'orange'];
+
+let randomValues: boolean[] = [true, false];
+
+console.log(fruits);
+
+let mixedValues: (number | string)[] = ['hello', 123];
+
+let names = ['peter', 'susan', 1];
+
+let array: (string | boolean)[] = ['name', true];
+
+// challenges
+let temperatures: number[] = [20, 25, 30];
+
+// temperatures.push('hot')
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+// colors.push(true)
+
+let mixedArray: (number | string)[] = ['helelo', 1];
