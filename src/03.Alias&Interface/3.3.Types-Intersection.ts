7a37d978dfd722919af4e5ff7914d315f0f1aad5
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-18 14:25:18 +0600

    interface explored

---
+++ src/03.Alias&Interface/3.3.Types-Intersection.ts
@@ -0,0 +1,31 @@
+// type Book = { id: number; name: string; price: number };
+
+// type DiscountedBook = Book & { discount: number };
+
+// const book1: Book = {
+//   id: 1,
+//   name: 'Book 1',
+//   price: 10.99,
+// };
+
+// const book2: Book = {
+//   id: 2,
+//   name: 'Book 2',
+//   price: 15.99,
+// };
+
+// const discountedBook: DiscountedBook = {
+//   id: book1.id,
+//   name: book1.name,
+//   price: 25,
+//   discount: 0.15,
+// };
+
+
+// const propName = 'age';
+
+// type Animal = {
+//   [propName]: number;
+// };
+
+// let tiger: Animal = { [propName]: 5 };
