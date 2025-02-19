0ee9718c75f954a4f990a419de12733944e8585e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-19 11:30:22 +0600

    enum explored with example

---
+++ src/chapters/04.Tuple&Enum/4.2.enumExample.ts
@@ -0,0 +1,19 @@
+enum ServerResponseStatus {
+  Success = 200,
+  Error = 'Error',
+}
+
+interface ServerResponse {
+  result: ServerResponseStatus;
+  data: string[];
+}
+
+function getServerResponse(): ServerResponse {
+  return {
+    result: ServerResponseStatus.Success,
+    data: ['first item', 'second item'],
+  };
+}
+
+const response: ServerResponse = getServerResponse();
+console.log(response);
