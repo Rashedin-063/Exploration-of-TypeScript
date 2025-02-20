2b00175aa798f1e9c92d588523946c16198681ea
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-20 15:20:05 +0600

    generic exploration starts

---
+++ src/6.Generics/6.1.GenericFundamental.ts
@@ -0,0 +1,9 @@
+// In TypeScript, you can declare an array using two syntaxes:
+
+// let array1: string[] = ['Apple', 'Banana', 'Mango'];
+// let array2: number[] = [1, 2, 3];
+// let array3: boolean[] = [true, false, true];
+
+let array1: Array<string> = ['Apple', 'Banana', 'Mango'];
+let array2: Array<number> = [1, 2, 3];
+let array3: Array<boolean> = [true, false, true];
