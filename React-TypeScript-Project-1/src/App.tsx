a642929b90925af9cc9270dbdc06a053eb1b12eb
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-01 10:50:06 +0600

    react ts props explored

--- React-TypeScript-Project-1/src/App.tsx
+++ React-TypeScript-Project-1/src/App.tsx
@@ -1,9 +1,14 @@
 import Component from './starter/02-props'
 
+
+const child = <>
+<p>This is a children</p>
+</>
+
 function App() {
   return (
     <main>
-     <Component/>
+      <Component name='peter' id={123} children={child} />
     </main>
   );
 }
--- React-TypeScript-Project-1/src/starter/02-props/index.tsx
+++ React-TypeScript-Project-1/src/starter/02-props/index.tsx
@@ -1,8 +1,26 @@
-function Component() {
+import { type PropsWithChildren } from "react";
+
+
+// type ComponentProps = {
+//   name: string;
+//   id: number;
+//   children?: React.ReactNode
+// };
+
+type ComponentProps = PropsWithChildren<{
+    name: string;
+    id: number;
+  }>
+
+function Component({ name, id , children }: ComponentProps) {
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Props</h2>
+      <h3>Name : {name}</h3>
+      <h3>ID : {id}</h3>
+
+     {children}
     </div>
   );
 }
