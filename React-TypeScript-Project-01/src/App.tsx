cf3f1ceedbb10231ec9a2d8787befc7d18f64078
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-06 11:01:08 +0600

    react ts function as props explored

--- React-TypeScript-Project-01/src/App.tsx
+++ React-TypeScript-Project-01/src/App.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import Counter from "./components/Counter"
+import Heading from "./components/Heading"
+import Section from "./components/Section"
+
 const App = () => {
+  const [count, setCount] = useState<number>(1);
+  
   return (
-    <div>App</div>
+    <div>
+      <Heading title='Hello Title' />
+      <Section title="Different Title">
+        <p>This is a section</p>
+      </Section>
+      <Counter setCount={setCount}>
+        Count is {count}</Counter>
+    </div>
   )
 }
 export default App
