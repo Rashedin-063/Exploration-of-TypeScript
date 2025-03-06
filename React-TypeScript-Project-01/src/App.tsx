47ee0b44a621a6470c48c6bcb72be13eeaaaef39
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-06 11:17:25 +0600

    react typescript with dave gray completed

--- React-TypeScript-Project-01/src/App.tsx
+++ React-TypeScript-Project-01/src/App.tsx
@@ -2,6 +2,32 @@ import { useState } from "react";
 import Counter from "./components/Counter"
 import Heading from "./components/Heading"
 import Section from "./components/Section"
+import List from "./components/List";
+
+const items = [
+  'Apple',
+  'Banana',
+  'Cherry',
+  'Date',
+  'Elderberry',
+  'Fig',
+  'Grape',
+  'Honeydew',
+  'Kiwi',
+  'Lemon',
+  'Mango',
+  'Nectarine',
+  'Orange',
+  'Pineapple',
+  'Quince',
+  'Raspberry',
+  'Strawberry',
+  'Tangerine',
+  'Uva',
+  'Watermelon',
+  'Yankee',
+  'Zucchini', // Add more fruits as needed!
+];
 
 const App = () => {
   const [count, setCount] = useState<number>(1);
@@ -14,6 +40,7 @@ const App = () => {
       </Section>
       <Counter setCount={setCount}>
         Count is {count}</Counter>
+      <List items={items} render={(item: string) => <span className="gold">{item}</span>} />
     </div>
   )
 }
