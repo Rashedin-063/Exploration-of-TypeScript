a32e6386c4dc9bedf0f6cf058fca0cd7df0007e1
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-08 12:55:55 +0600

    useCallback and useMemo hooks explored with typeScript

--- React-TypeScript-Project-01/src/App.tsx
+++ React-TypeScript-Project-01/src/App.tsx
@@ -1,9 +1,20 @@
-import { useEffect, useState } from "react"
+import { MouseEvent, useCallback, useEffect, useState, useMemo } from "react"
 
 interface User {
   id: number;
   userName: string
 }
+
+
+type FibFunction = (n: number) => number ;
+
+const fib:FibFunction = (n) => {
+  if (n < 2) return n;
+
+  return fib(n-1) + fib(n-2);
+}
+
+const myNum: number = 27;
 
 
 const App = () => {
@@ -18,10 +29,18 @@ const App = () => {
     return () => console.log('unmount');
     
     
-  },[users])
+  }, [users])
+  
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent): void => setCount(prev => prev + 1), [])
+
+  const result = useMemo<number>(() => fib(myNum), [])
 
   return (
-    <div>App</div>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '90vh'}}>
+      <h1 style={{textAlign: 'center', marginBottom: '1rem'}}>{count}</h1>
+      <button onClick={addTwo}>Increment</button>
+      <h2>{result}</h2>
+    </div>
   )
 }
 export default App
