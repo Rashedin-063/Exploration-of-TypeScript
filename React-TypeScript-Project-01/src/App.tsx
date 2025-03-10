ede9c2e4524615e869e73cdcc1a5332f8690e326
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-11 00:06:49 +0600

    updated

--- React-TypeScript-Project-01/src/App.tsx
+++ React-TypeScript-Project-01/src/App.tsx
@@ -1,60 +1,6 @@
-import { MouseEvent, useCallback, useEffect, useState, useMemo, useRef } from "react"
-
-interface User {
-  id: number;
-  userName: string
-}
-
-
-type FibFunction = (n: number) => number ;
-
-const fib:FibFunction = (n) => {
-  if (n < 2) return n;
-
-  return fib(n-1) + fib(n-2);
-}
-
-const myNum: number = 27;
-
-
 const App = () => {
-  const [count, setCount] = useState<number>(0)
-  const [users, setUsers] = useState<User[] | null>(null);
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  console.log(inputRef.current);
-
-  console.log((inputRef?.current?.value));
-  
-  
-
-  // useEffect(() => {
-  //   console.log('mounting');
-  //   console.log('Users:', users);
-    
-
-  //   return () => console.log('unmount');
-    
-    
-  // }, [users])
-  
-  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent): void => setCount(prev => prev + 2), [])
-
-  const result = useMemo<number>(() => fib(myNum), [])
-
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '90vh'}}>
-      <h1 style={{textAlign: 'center', marginBottom: '1rem'}}>{count}</h1>
-      <button onClick={addTwo}>Increment</button>
-      <h2>{result}</h2>
-
-
-      <>
-      <input ref={inputRef} type='text'/>
-      </>
-
-    </div>
+    <div>App</div>
   )
 }
 export default App
