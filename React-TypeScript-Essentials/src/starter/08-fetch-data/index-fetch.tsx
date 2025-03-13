0fc5347b18a440da4234850783c6c6a7eb1bc4e2
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-13 23:02:45 +0600

    data fetched using axios

---
+++ React-TypeScript-Essentials/src/starter/08-fetch-data/index-fetch.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { type Tour, tourSchema } from './types';
+
+function Component() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+  const [tours, setTours] = useState<Tour[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const rawData: Tour[] = await response.json();
+        const result = tourSchema.array().safeParse(rawData);
+
+        if (!result.success) {
+          console.log(result.error.message);
+          throw new Error('Failed to parse tours');
+        }
+        setTours(result.data);
+      } catch (error) {
+        const message =
+          error instanceof Error ? error.message : 'There was an error...';
+        setIsError(message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return <h3>Error: {isError}</h3>;
+  }
+
+  return (
+    <div>
+      <h2 className='mb-1'>Tours</h2>
+      {tours.map((tour) => {
+        return (
+          <h3 key={tour.id} style={{ marginBottom: '40px' }}>
+            {tour.name}
+          </h3>
+        );
+      })}
+    </div>
+  );
+}
+export default Component;
--- React-TypeScript-Essentials/src/starter/08-fetch-data/types.ts
+++ React-TypeScript-Essentials/src/starter/08-fetch-data/types.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
+import axios from 'axios';
 
+
+const url = 'https://www.course-api.com/react-tours-project';
 
 export const tourSchema = z.object({
   id: z.string(),
@@ -11,3 +14,17 @@ export const tourSchema = z.object({
 })
 
 export type Tour = z.infer<typeof tourSchema>
+
+
+export const fetchTours = async (): Promise<Tour[]> => {
+  const response = await axios.get<Tour[]>(url);
+  const result = tourSchema.array().safeParse(response.data);
+  
+   if (!result.success) {
+     console.log(result.error.message);
+     throw new Error('Failed to parse tours');
+  }
+  
+  return result.data;
+}
+
