18de8fe1351182f29e549ec8e72059ae82e2d21e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-13 14:17:43 +0600

    data fetched from database

--- React-TypeScript-Essentials/src/starter/08-fetch-data/index.tsx
+++ React-TypeScript-Essentials/src/starter/08-fetch-data/index.tsx
@@ -1,4 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const url = 'https://www.course-api.com/react-tours-project';
+
 function Component() {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState<string | null>(null)
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const rawData = await response.json()
+        console.log(rawData);
+        return rawData
+        
+      } catch (error) {
+        const message = error instanceof Error ? error.message : 'There was an error...';
+        setIsError(message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return <h3>Error: {isError}</h3>;
+  }
+
   return (
     <div>
       <h2>React & Typescript</h2>
