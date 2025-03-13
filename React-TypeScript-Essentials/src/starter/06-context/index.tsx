e9223030c25556e827ebf71c80d992e42f8bf0a6
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-13 10:22:36 +0600

    updated

--- React-TypeScript-Essentials/src/starter/06-context/index.tsx
+++ React-TypeScript-Essentials/src/starter/06-context/index.tsx
@@ -2,32 +2,17 @@ import { ThemeProvider, useTheme } from "./context";
 
 
 
-function ParentComponent() {
-  return <ThemeProvider>
-    <Component/>
-  </ThemeProvider>
-}
 
 function Component() {
 
-  const context = useTheme()
-
-  console.log(context);
   
 
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Context API</h2>
-      <button onClick={() => {
-        if (context.theme === 'dark') {
-          context.setTheme('system');
-          return;
-        }
-        context.setTheme('dark');
-      }} className="btn btn-center">Toggle Theme</button>
     </div>
   );
 }
 
-export default ParentComponent;
+export default Component;
