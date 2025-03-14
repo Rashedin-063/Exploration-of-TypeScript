bd6645479c3308ee6e869d568878c7363a31d9f9
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-14 22:56:47 +0600

    redux counterSlice created with ts

---
+++ React-TypeScript-Essentials/src/starter/09-rtk/counterSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+
+type CounterStatus = 'pending...' | 'active' | 'inactive'
+
+type CounterState = {
+  count: number,
+  status: CounterStatus
+}
+
+const initialState: CounterState = {
+  count: 0,
+  status: 'pending...'
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1
+    },
+    decrement: (state) => {
+      state.count -= 1
+    },
+    reset: (state) => {
+      state.count = 0
+    },
+    setStatus: (state, action: PayloadAction<CounterStatus>) => {
+     state.status = action.payload
+   }
+  }
+
+
+})
+
+export default counterSlice.reducer
+export const {increment, decrement, reset, setStatus} = counterSlice.actions
