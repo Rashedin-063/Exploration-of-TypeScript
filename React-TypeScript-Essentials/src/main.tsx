958a2cb7d12179869074df89425ab3af7fa8a5bf
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-15 14:19:20 +0600

    hooks setup and app wrapped with Provider

--- React-TypeScript-Essentials/src/main.tsx
+++ React-TypeScript-Essentials/src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { store } from './store.ts';
 
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </QueryClientProvider>
 );
